f283c31c34df28cc37a8415ea22540386db83402
vladmosoiu98 <vlad.m@agorarobotics.com>
2023-10-20 16:43:06 +0300

    depthai oak camera support - config and launch

---
+++ isaac_ros_apriltag/launch/isaac_ros_apriltag_oak.launch.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
+# Copyright (c) 2021-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    oak_params_path = os.path.join(
+        get_package_share_directory('isaac_ros_apriltag'),
+        'config',
+        'oak_params.yaml'
+    )
+    
+    depthai_prefix = get_package_share_directory('depthai_ros_driver')
+    remappings=[('/image', '/oak/rgb/image_raw'), 
+                ('/camera_info', '/oak/rgb/camera_info')]
+    
+    rectify_node = ComposableNode(
+        package='isaac_ros_image_proc',
+        plugin='nvidia::isaac_ros::image_proc::RectifyNode',
+        name='rectify',
+        namespace='',
+        parameters=[{
+            'output_width': 1920,
+            'output_height': 1080,
+        }]
+    )
+
+    apriltag_node = ComposableNode(
+        package='isaac_ros_apriltag',
+        plugin='nvidia::isaac_ros::apriltag::AprilTagNode',
+        name='apriltag',
+        namespace='',
+        remappings=remappings
+    )
+    
+    depthai = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(depthai_prefix, 'launch', 'camera.launch.py')),
+            launch_arguments={"name": 'oak',
+                              "params_file": oak_params_path,
+                               }.items())
+
+    apriltag_container = ComposableNodeContainer(
+        package='rclcpp_components',
+        name='apriltag_container',
+        namespace='',
+        executable='component_container_mt',
+        composable_node_descriptions=[
+            rectify_node,
+            apriltag_node
+        ],
+        output='screen'
+    )
+
+    return launch.LaunchDescription([depthai, apriltag_container])
